280024a416e05c32b6205c88ebb78dbb4d879857
Romeo De Timmerman <romeo.detimmerman@gmail.com>
2025-07-04 16:14:20 +0200

    updating data utils

--- src/features/utils.py
+++ src/features/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 """
@@ -58,3 +59,17 @@ def calculate_relative_time_points(token, duration_column, time_points):
         time_values[key] = point * duration
 
     return time_values
+
+
+def normalize_speaker_formants(df, speaker_col="artist", formant_cols=["f1p", "f2p"]):
+    """
+    z-score normalize formant columns per speaker
+    """
+    df = df.copy()
+    for speaker, group in df.groupby(speaker_col):
+        for col in formant_cols:
+            mean = group[col].mean()
+            std = group[col].std()
+            idx = group.index
+            df.loc[idx, col] = (group[col] - mean) / std if std > 0 else 0
+    return df
